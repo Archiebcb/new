4d55b7a0c4722b63356c6d3b30c1a3d84577a1bf
archie <archiecope@mac.home>
2025-03-15 22:55:44 +0000

    third

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,8 +8,12 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
-
-export default function Form({ customers }: { customers: CustomerField[] }) {
+ 
+export default function Form({
+  customers,
+}: {
+  customers: CustomerField[];
+}) {
   return (
     <form action={createInvoice}>
 
